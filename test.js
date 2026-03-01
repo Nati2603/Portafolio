eebd35f1c39acec75413aab156a848e2261e5dd4
Nati2603 <nati.m.26366@gmail.com>
2026-03-01 14:52:20 -0600

    Detalles diseño, scroll

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log("Hola Mundo")
+/*Modo oscuro/claro*/
+const toggleBtn = document.getElementById("theme-toggle");
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    if (document.body.classList.contains("dark")) {
+        toggleBtn.textContent = "☀️";
+    } else {
+        toggleBtn.textContent = "🌙";
+    }
+  });
